7cc86117d905e254f5781e8a78372cc929682da3
Evollution-FTC <evolution.ftc.2024.2025@gmail.com>
2025-01-09 19:33:05 -0500

    Update Climber.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Climber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Climber.java
@@ -1,1 +1,16 @@
+package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Climber {
+
+    private DcMotor motor;
+
+    public Climber(DcMotor motor){
+        this.motor = motor;
+
+    }
+    public void setSpeed(double power){
+        motor.setPower(power);
+    }
+}
