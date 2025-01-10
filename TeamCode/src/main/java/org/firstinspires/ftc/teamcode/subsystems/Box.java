77cb84d6e0b7cd377e4fcc98e7bd442baadfdd80
Evollution-FTC <evolution.ftc.2024.2025@gmail.com>
2025-01-09 19:32:12 -0500

    Update Box.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Box.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Box.java
@@ -1,1 +1,16 @@
+package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class Box {
+
+    private CRServo servo;
+
+    public Box(CRServo servo){
+        this.servo = servo;
+
+    }
+    public void setSpeed(double power){
+        servo.setPower(power);
+    }
+}
