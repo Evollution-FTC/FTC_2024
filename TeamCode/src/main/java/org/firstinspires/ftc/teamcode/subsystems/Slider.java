9bb70e570b168f78a707502f309c01b585a58459
Evollution-FTC <evolution.ftc.2024.2025@gmail.com>
2025-01-09 19:30:23 -0500

    Update Slider.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Slider.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Slider.java
@@ -1,1 +1,17 @@
+package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Motor;
+
+public class Slider {
+
+    private DcMotor motor;
+
+    public Slider(DcMotor motor){
+        this.motor = motor;
+
+    }
+    public void setSpeed(double power){
+        motor.setPower(power);
+    }
+}
